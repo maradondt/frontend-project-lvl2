7f4833f06f17e28156b1177661e869921b162ff6
maradondt <maradondt@gmail.com>
2021-01-27 14:46:12 +0300

    Настройка окружения

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import program from 'commander';
+// import fs from 'fs';
+// console.log(JSON.parse(fs.readFileSync('frontend-project-lvl2/package.json')).version);
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the current version');
+
+program.parse();
